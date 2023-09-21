0b69d738a511875f0cc0f83586a3935a91453cf8
Nirodha Perera <nirodha@ascentic.se>
2023-09-21 14:57:09 +0530

    Added semantics label to rounded button

--- lib/buttons/rounded_button.dart
+++ lib/buttons/rounded_button.dart
@@ -11,7 +11,8 @@ class RoundedButton extends StatelessWidget {
       this.padding = 13.0,
       required this.onPress,
       this.fontWeight = FontWeight.w900,
-      this.height = 53});
+      this.height = 53,
+      this.semanticsLabel});
 
   final String btnTitle;
   final Color bgColor;
@@ -22,23 +23,25 @@ class RoundedButton extends StatelessWidget {
   final FontWeight? fontWeight;
   final double? height;
   final double? width;
+  final String? semanticsLabel;
 
   @override
   Widget build(BuildContext context) {
     return MaterialButton(
+      key: key,
       height: height,
       onPressed: () => onPress != null ? onPress!() : null,
-      minWidth: (MediaQuery.of(context).size.width) /
-          (width == null ? 0 : width!),
+      minWidth:
+          (MediaQuery.of(context).size.width) / (width == null ? 0 : width!),
       padding: EdgeInsets.all(padding),
       colorBrightness: Brightness.dark,
       color: onPress != null ? bgColor : const Color(0xFFC4C4C4),
       elevation: 1.0,
       highlightElevation: 1.0,
-      shape: RoundedRectangleBorder(
-          borderRadius:  BorderRadius.circular(30)),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
       child: Text(
         btnTitle,
+        semanticsLabel: semanticsLabel,
         style: TextStyle(
             color: textColor, fontSize: fontSize, fontWeight: fontWeight),
       ),
